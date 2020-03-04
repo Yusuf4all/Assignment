eb09bad3411dadb94da1aa5d35775838d1dcd899
Yusuf Khan <53833793+Yusuf4all@users.noreply.github.com>
2020-03-04 12:21:18 +0530

    Add files via upload

---
+++ precache-manifest.01c641303e51fbb83f7f95f30b507c4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2b7b66e4488130ba9d748ed25ef323",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed7f1ecaabe37df974a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d83e45d29739b38c65ef",
+    "url": "/static/css/main.2600446b.chunk.css"
+  },
+  {
+    "revision": "5ed7f1ecaabe37df974a",
+    "url": "/static/js/2.19b9d63e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.19b9d63e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83e45d29739b38c65ef",
+    "url": "/static/js/main.9997ab0a.chunk.js"
+  },
+  {
+    "revision": "49b1bb9cd732ee06a606",
+    "url": "/static/js/runtime-main.c8ff005a.js"
+  }
+]);
